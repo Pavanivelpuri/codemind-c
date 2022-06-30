06b668d8efbded4cfa126839cade2b2e7c1030c0
Pavanivelpuri <98639057+Pavanivelpuri@users.noreply.github.com>
2022-06-30 23:42:11 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             if(i%2==0)             {                 count++;                 break;             }         }     }     if(count==0)     printf("True");     else     printf("False");     return 0; }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int x[20],n,i,flag=0;
+    int n,i,count=0;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    scanf("%d",&x[i]);
+    int arr[n];
     for(i=0;i<n;i++)
     {
-        if(x[i]%2!=0)
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
         {
             if(i%2==0)
             {
-             flag=1;
-             break;
+                count++;
+                break;
             }
         }
     }
-    if(flag==0)
+    if(count==0)
     printf("True");
     else
     printf("False");
